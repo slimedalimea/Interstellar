04c410aced46a0eb5cfa7abcca445d79ff4f44aa
slimedalimea <100090330+slimedalimea@users.noreply.github.com>
2024-09-05 00:11:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+const config = {
+  challenge: true, // Set to true if you want to enable password protection.
+  blocked: {
+    // Normal users can ignore this option. This is for the owner of the website.
+    "gointerstellar.app": "",
+    localhost: "",
+  },
+  users: {
+    // You can add multiple users by doing username: 'password'.
+    admin: "iamnotchangingbro",
+  },
+};
+
+export default config;
 
